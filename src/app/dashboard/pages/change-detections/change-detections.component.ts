2ffa8a4fdcaac2d9dc84ff0277a705b71952348b
SebastianCamacho <jscamacho@unicesar.edu.co>
2024-02-27 16:53:41 -0500

    uso de signals para renderizar componentes indivuduales, y change detections

--- src/app/dashboard/pages/change-detections/change-detections.component.ts
+++ src/app/dashboard/pages/change-detections/change-detections.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { TitleComponent } from "../../../shared/title/title.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-change-detections',
     standalone: true,
-    templateUrl: './change-detections.component.html',
-    imports: [TitleComponent]
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [TitleComponent, CommonModule],
+    template:`
+    <app-title [titulo]="currentFramework()"></app-title>
+
+        <pre>{{frameworkAsSignal() | json }}</pre>
+
+        <pre>{{ frameworkAsProperity | json }}</pre>
+    `
+    
 })
 export default class ChangeDetectionsComponent {
+
+    public currentFramework = computed(
+        () => `Change detection - ${this.frameworkAsSignal().name}`
+    );
+
+    public frameworkAsSignal = signal({
+        name: 'Angular',
+        releaseData: 2016
+    });
+
+    public frameworkAsProperity = {
+        name: 'Angular',
+        releaseData: 2016
+    };
+
+    constructor() {
+
+        setTimeout(() => {
+
+            //this.frameworkAsProperity.name = 'React';
+            this.frameworkAsSignal.update(value => ({...value, name:'React'}))
+            console.log('Hecho')
+
+        }, 3000)
+
+
+    }
+
 
 }
